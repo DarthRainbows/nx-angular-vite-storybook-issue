76f6b27644301f527853cbfbb8499df731308bb2
DarthRainbows <5976786+DarthRainbows@users.noreply.github.com>
2025-04-28 17:36:08 -0400

    removed core configuration from storybook

--- libs/foo/.storybook/main.ts
+++ libs/foo/.storybook/main.ts
@@ -3,9 +3,6 @@ import type { StorybookConfig } from '@analogjs/storybook-angular';
 const config: StorybookConfig = {
   stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: ['@storybook/addon-essentials'],
-  core: {
-    builder: '@storybook/builder-vite',
-  },
   framework: {
     name: '@analogjs/storybook-angular',
     options: {},
